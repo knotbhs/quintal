e2fed81e5b9b2f5a69ddc63b4d548a0ea4e3464b
Bruno Scarpari <knotbhs@gmail.com>
2022-07-18 19:19:26 -0300

    Correção de Metricas de Pesquisa

--- resources/views/components/nav-recibos.blade.php
+++ resources/views/components/nav-recibos.blade.php
@@ -12,10 +12,10 @@
     {!! Form::open([
         'url' => route('recibos.search.all'),
         'method' => 'POST',
-        'class' => 'd-flex',
+        'class' => 'd-flex ',
         'id' => 'formFindDate',
     ]) !!}
-    <div class="input-group">
+    <div class="input-group {{ $btnactive == 'recibo' ? 'd-none' : '' }}">
         <input style="max-width: 130px;" onchange="findWithDate()" autocomplete="off" name="data_start"
             value="{{ isset($datastart) ? $datastart : '' }}" class="form-control me-2 data" type="search"
             placeholder="Data Inicial" aria-label="Search">
@@ -23,9 +23,9 @@
             value="{{ isset($dataend) ? $dataend : '' }}" name="data_end" class="form-control me-4 data"
             type="search" placeholder="Data Final" aria-label="Search">
     </div>
-    <div class="input-group border-start border-secondary">
+    <div class="input-group border-start border-secondary  {{ $btnactive == 'recibo' ? 'd-none' : '' }}">
         <select name="pesquisarpor" class="form-select form-control me-2 ms-4" aria-label="Default select example">
-            <option>Pesquisar por:</option>
+            <option value="">Pesquisar por:</option>
             <option {{ $pesquisarpor == 'colaborador' ? 'selected' : '' }} value="colaborador">Colaborador</option>
             <option {{ $pesquisarpor == 'servico' ? 'selected' : '' }} value="servico">Serviço Prestado</option>
             <option {{ $pesquisarpor == 'all' ? 'selected' : '' }} value="all">Ambos</option>
